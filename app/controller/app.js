b2d240211bf45fa0f98d4004e110cecad019ed29
Ryan Tablada <ryan.tablada@gmail.com>
2017-02-23 11:20:34 -0600

    Setup app controller

---
+++ app/controller/app.js
@@ -0,0 +1,27 @@
+// app/controller/app.js
+
+export default class AppController {
+  constructor(el, store) {
+    this.el = el;
+    this.store = store;
+
+    // Maybe initialize some other classes/objects
+  }
+
+  // We'll manually fire this when the app is done being created
+  created() {
+    // Listen for changes in the store and save them in local storage
+    this.store.subscribe(() => {
+      // Save the list of snacks as a JSON String in localstorage
+      const snacks = this.store.getState().snacks;
+      window.localStorage.snacks = JSON.stringify(snacks);
+    });
+
+    // Get the stringified list of snacks or a default of an empty array
+    const dataString = window.localStorage.snacks || '[]';
+    // Dispatch FIND_ALL to the store with the data loaded from localstorage
+    this.store.dispatch({ type: 'SNACK@FIND_ALL', data: JSON.parse(dataString) });
+
+    // "mount" (Start up) views
+  }
+}
--- app/index.js
+++ app/index.js
@@ -1,7 +1,14 @@
 import 'whatwg-fetch';
+import store from './store';
+import AppController from './controller/app';
 
 // Look up the app element
 const appEl = document.querySelector('.app');
+
+// Make a new app controller
+const app = new AppController(appEl, store);
+// Start the app controller
+app.created();
 
 // Hook up the form to listen for submit events and dispatch an action to redux
 // Get all of the snacks and create list items for each one
